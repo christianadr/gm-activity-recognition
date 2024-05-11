faa670cecd2727c369758c5ca816495f08b1576a
Christian Del Rosario <ainsley.drosario@gmail.com>
2024-05-11 18:23:16 +0800

    [data] Generate video annotations and splitted videos into train and validation splits

--- src/constants/globals.py
+++ src/constants/globals.py
@@ -1,5 +1,11 @@
 from pathlib import Path
 
 PROJECT_DIR = Path(*Path(__file__).parts[:-3])
+
+# DATASETS
 RAW_DIR = Path(PROJECT_DIR, "data", "raw")
 PROCESSED_DIR = Path(PROJECT_DIR, "data", "processed")
+
+GROSSMOTOR_DIR = Path(PROJECT_DIR, "data", "grossmotor")
+TRAIN_DIR = Path(GROSSMOTOR_DIR, "train")
+VAL_DIR = Path(GROSSMOTOR_DIR, "val")
